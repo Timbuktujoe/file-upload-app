c378137e4e25d77c8bafa57328c07f1f60bf8c05
Timbuktujoe <23214407+Timbuktujoe@users.noreply.github.com>
2022-03-03 19:24:39 +0100

    add POST Route

--- index.js
+++ index.js
@@ -22,5 +22,26 @@ app.use('/a', express.static('/b'));
 app.use(express.static(__dirname + '/public'));
 app.use('/uploads', express.static('uploads'));
 
+app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+    // req.file is the `profile-file` file
+    // req.body will hold the text fields, if there were any
+    console.log(JSON.stringify(req.file))
+    var response = '<a href="/">Home</a><br>'
+    response += "Files uploaded successfully.<br>"
+    response += `<img src="${req.file.path}" /><br>`
+    return res.send(response)
+})
+
+app.post('/profile-upload-multiple', upload.array('profile-files', 12), function (req, res, next) {
+    // req.files is array of `profile-files` files
+    // req.body will contain the text fields, if there were any
+    var response = '<a href="/">Home</a><br>'
+    response += "Files uploaded successfully.<br>"
+    for (var i = 0; i < req.files.length; i++) {
+        response += `<img src="${req.files[i].path}" /><br>`
+    }
+
+    return res.send(response)
+})
 
 app.listen(port, () => console.log(`Server running on port ${port}!`))
